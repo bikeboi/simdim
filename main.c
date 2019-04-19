bf67e811d27ea8c1f73792f2c119f5eee7c7e047
kine <kirilos24@gmail.com>
2019-04-20 00:11:05 +0100

    Code style && basic config parsing

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
+
 
 #define MAX_DM_COUNT 7
 
 typedef struct {
-  char* name;
-  char* path;
+  char *name;
+  char *path;
 } DM;
 
-int read_into_dms(FILE *f, DM *dms);
-void show_dms(DM *dms, int n);
+int read_into_dms(FILE *f, DM **dms);
+void show_dms(DM **dms, int n);
 
 const char *rc_file = "dmanrc";
 
@@ -22,8 +24,7 @@ int main() {
 		//
 		conf = malloc (
 			// /home/xyz + / +         .config   + \0
-			strlen(home) + 1 + strlen(".config") + 1
-		);
+			strlen(home) + 1 + strlen(".config") + 1);
 		sprintf(conf, "%s/%s%c", home, ".config", 0);
 	}
 	printf("Config home: %s\n", conf);
@@ -38,37 +39,40 @@ int main() {
 	if (file == NULL || dms == NULL) {
 		printf("RIP");
 	} else {
-		int num_read = read_into_dms(file,dms);
-		show_dms(dms,num_read);
+		int num_read = read_into_dms(file, &dms);
+		printf("Read %i entries\n", num_read);
+		show_dms(&dms,num_read);
 	}
 	//
 	free(dms);
 	return 0;
 }
 
-int read_into_dms(FILE* f, DM* dms) {
-  int i;
-  for(i = 0; !feof(f) && i < MAX_DM_COUNT; ++i) {
-    // Will these get deallocated out of scope? Please no
-    char name[100];
-    char path[100];
+int read_into_dms(FILE *f, DM **dms) {
+	size_t counted = 0;
+	size_t read = 0;
+	char *line;
+	while((read=getline(&line, &read, f)) != -1) {
+		size_t i, linelen = strlen(line);
+		for(i=0; i < linelen && isspace(line[i]); i++) {}
+		int notempty = linelen - i > 0;
+		if(notempty) {
+			sscanf(line, " %m[^:]: %ms ", &dms[counted]->name, &dms[counted]->path); 
+			printf("Read %lu (%zu) :: Name = <%s>, Path = <%s>\n", 
+					read, counted, (dms[counted])->name, (dms[counted])->path);
 
-    fscanf(f," %s : %s ",name,path);
-
-    (dms+i)->name = name;
-    (dms+i)->path = path;
-
-    printf("Read %i :: Name = %s, Path = %s\n", i, (dms+i)->name, (dms+i)->path);
-  }
-  printf("Found %i display-manager entries\n", i);
-  return i;
+			counted++;
+		}
+	}
+	printf("Found %zu display-manager entries\n", counted);
+	return counted;
 }
 
-void show_dms(DM* dms, int n) {
+void show_dms(DM **dms, int n) {
   int i;
   printf("--\n");
   for(i = 0; i < n; ++i) {
-    printf("Read %i :: Name = %s, Path = %s\n", i, (dms+i)->name, (dms+i)->path);
-    printf("%s", (dms+i)->path);
+    printf("Read %i :: Name = %s, Path = %s\n", i, dms[i]->name, dms[i]->path);
+    printf("%s\n", (dms[i])->path);
   }
 }
